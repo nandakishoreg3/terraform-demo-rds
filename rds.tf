b483ceabb63f9b3aba06105a03c56c8cf50649c4
nanda <nanda_kishore@hotmail.com>
2024-02-21 01:45:40 +0530

    first commit

---
+++ rds.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.36"
+    }
+  }
+}
+provider "aws" {
+  }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
